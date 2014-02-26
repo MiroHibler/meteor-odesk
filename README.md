a39abe02edab8aebc7b5498dc82b26867f6cda9a
Miroslav Hibler <mhibler@gmail.com>
2014-02-26 14:38:00 +0100

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# meteor-odesk
+
+Meteor oDesk OAuth Flow.
+
+
+## Dependencies
+
+ * Meteor v0.7.1+
+
+
+## TL;DR;
+
+_meteor-odesk_ enables login with [oDesk](http://odesk.com) account.
+
+## Installation
+
+Install using [Meteorite](https://github.com/oortcloud/meteorite) - Installer & smart package manager for Meteor:
+
+```sh
+$ mrt add odesk
+```
+
+## Changelog
+
+### v0.1.0
+ * Initial release
+
+## Copyright and license
+
+Copyright © 2014 [Miroslav Hibler](http://miro.hibler.me)
+
+_meteor-odesk_ is licensed under the [**MIT**](http://miro.mit-license.org) license.
---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+	summary: "oDesk OAuth Flow"
+});
+
+Package.on_use( function( api ) {
+	api.use( 'http', ['client', 'server'] );
+	api.use( 'templating', 'client' );
+	api.use( 'oauth1', ['client', 'server'] );
+	api.use( 'oauth', ['client', 'server'] );
+	api.use( 'random', 'client' );
+	api.use( 'underscore', 'server' );
+	api.use( 'service-configuration', ['client', 'server'] );
+
+	// Service Name has to be capitalized
+	api.export( 'Odesk' );
+
+	api.add_files( ['lib/odesk_configure.html', 'lib/odesk_configure.js'], 'client' );
+
+	api.add_files( 'lib/server/odesk.js', 'server' );
+	api.add_files( 'lib/client/odesk.js', 'client' );
+});
